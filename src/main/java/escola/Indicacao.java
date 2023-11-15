481a6f1d54ce2bff174d948e1008044d63f4911a
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2023-11-15 13:03:08 -0300

    feat: create student builder

---
+++ src/main/java/escola/Indicacao.java
@@ -0,0 +1,19 @@
+package escola;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class Indicacao {
+
+    private Aluno indicado;
+    private Aluno indicante;
+    private LocalDateTime dataIndicacao;
+
+    public Indicacao(Aluno indicado, Aluno indicante) {
+        this.indicado = indicado;
+        this.indicante = indicante;
+        this.dataIndicacao = LocalDateTime.now();
+    }
+}
