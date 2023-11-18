3e7f17790577711fa60fbcc9adfcd48caa1b9f39
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2023-11-18 13:26:06 -0300

    feat: implement students concrete repos

--- src/main/java/br/com/alura/escola/dominio/aluno/Aluno.java
+++ src/main/java/br/com/alura/escola/dominio/aluno/Aluno.java
@@ -1,8 +1,13 @@
 package br.com.alura.escola.dominio.aluno;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 public class Aluno {
 
     private CPF cpf;
